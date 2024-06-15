3ad1adca802fa4e9bab98c523311bce53df10617
Swapnil-DevGeek <f20220181@goa.bits-pilani.ac.in>
2024-06-15 13:25:24 +0530

    local storage call updated

--- app/page.js
+++ app/page.js
@@ -4,9 +4,13 @@ import { useEffect } from "react"
 
 export default function Home() {
 
-  useEffect(()=>{
+  useEffect(() => {
+    // Initialize localStorage only if it hasn't been initialized before
+    const items = JSON.parse(localStorage.getItem('items'));
+    if (!items) {
       initializeLocalStorage();
-  },[])
+    }
+  }, []);
 
 
   return (
